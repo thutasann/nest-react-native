28d28abc8ec160e39681eae28707d8ec273ffa40
thutasann <66902308+thutasann@users.noreply.github.com>
2023-07-24 17:03:03 +0630

    feat: add packages

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Client App</Text>
       <StatusBar style="auto" />
     </View>
   );
